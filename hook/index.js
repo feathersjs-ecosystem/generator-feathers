ce8c3e75883b23d39d377bfa85109220d4c8cafa
Eric Kryski <e.kryski@gmail.com>
2016-01-13 16:34:55 -0700

    adding a generator for promise based hooks

--- hook/index.js
+++ hook/index.js
@@ -30,9 +30,55 @@ module.exports = generators.Base.extend({
         ]
       },
       {
+        type: 'input',
+        name: 'service',
+        message: 'What service is this hook for?'
+      },
+      {
+        type: 'list',
+        name: 'method',
+        message: 'What method is this hook for?',
+        choices: [
+          {
+            name: 'no specific method',
+            value: null
+          },
+          {
+            name: 'all',
+            value: 'all'
+          },
+          {
+            name: 'find',
+            value: 'find'
+          },
+          {
+            name: 'get',
+            value: 'get'
+          },
+          {
+            name: 'create',
+            value: 'create'
+          },
+          {
+            name: 'update',
+            value: 'update'
+          },
+          {
+            name: 'patch',
+            value: 'patch'
+          },
+          {
+            name: 'remove',
+            value: 'remove'
+          }
+        ]
+      },
+      {
+        type: 'input',
         name: 'name',
         message: 'What do you want to call your hook?',
-      }
+      },
+      
     ];
 
     this.prompt(prompts, function (props) {
@@ -43,9 +89,14 @@ module.exports = generators.Base.extend({
   },
 
   writing: function () {
+    this.props.hookPath = path.join('server/hooks', this.props.service, this.props.type, this.props.method ? this.props.method : '', this.props.name + '.js');
+    
+    // TODO (EK): Automatically import the hook into hooks/index.js
+    // so that services can grab it easier.
+
     this.fs.copyTpl(
       this.templatePath(this.props.type + '-hook.js'),
-      this.destinationPath('server/hooks', this.props.name + '.js'),
+      this.destinationPath(this.props.hookPath),
       this.props
     );
     
--- hook/templates/before-hook.js
+++ hook/templates/before-hook.js
@@ -1,4 +1,15 @@
-export default function(hook, next) {
-  hook.data.ran = true;
-  next();
+// <%= hookPath %>.js
+// Use this hook to manipulate incoming data
+// or params before it is sent to the database.
+// For more information on hooks see: https://github.com/feathersjs/feathers-hooks
+
+export default function(hook) {
+  // Manipulate your data inside a promise
+  // so that you can chain them.
+  // See https://github.com/feathersjs/feathers-hooks#promises
+  return new Promise(function(resolve, reject) {
+    // make sure Id's don't get set manually
+    delete hook.data.id;
+    resolve(hook);
+  });
 }
