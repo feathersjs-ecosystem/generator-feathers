617d59e93a34a7b56b1cab64374f6c05af878377
Eric Kryski <e.kryski@gmail.com>
2016-01-12 21:02:17 -0700

    adding createdAt and updatedAt attributes to mongoose model

--- app/templates/models/mongoose-user.js
+++ app/templates/models/mongoose-user.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
   email: {type: String, required: true, index: true},
-  password: {type: String, required: true}
+  password: {type: String, required: true},
+  createdAt: {type: Date, 'default': Date.now},
+  updatedAt: {type: Date, 'default': Date.now}
 });
 
 let UserModel = mongoose.model('User', UserSchema);
