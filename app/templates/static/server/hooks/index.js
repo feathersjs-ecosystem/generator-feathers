c9b1b051159fdd75604ca62fe54675a1833cd8ec
Marshall Thompson <marshall@creativeideal.net>
2016-01-21 22:43:53 -0700

    Adds global hooks.
    Includes common hooks.  And auth hooks when applicable.

--- app/templates/static/server/hooks/index.js
+++ app/templates/static/server/hooks/index.js
@@ -1,4 +1,18 @@
-// jshint unused:false
-export default function() {
-  const app = this;
-}
+// Read all hooks from the file system and arrange them into a single object.
+import fs from 'fs';
+
+let hooks = {};
+
+fs.readdir(__dirname, (err, files) => {
+  files.forEach(fileName => {
+    if (fileName.indexOf('.js') >= 0 && fileName !== 'index.js') {
+      fileName = fileName.replace('.js', '');
+      let camelCased = fileName.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+      let module = require('./' + fileName);
+      module = module.default ? module.default : module;
+      hooks[camelCased] = module;
+    }
+  });
+});
+
+export default hooks;
