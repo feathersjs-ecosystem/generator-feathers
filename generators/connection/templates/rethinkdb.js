523ff8da952f4fcc232a093b9db846ec654c4395
David Luecke <daff@neyeon.com>
2017-02-18 15:15:42 -0800

    Add initialziation for RethinkDB databases and tables

--- generators/connection/templates/rethinkdb.js
+++ generators/connection/templates/rethinkdb.js
@@ -6,6 +6,25 @@ module.exports = function() {
   const app = this;
   const config = app.get('rethinkdb');
   const r = rethinkdbdash(config);
+  const oldSetup = app.setup;
 
   app.set('rethinkdbClient', r);
+
+  app.setup = function(...args) {
+    const result = oldSetup.apply(this, args);
+    
+    let promise = Promise.resolve();
+
+    // Go through all services and call the RethinkDB `init`
+    // which creates the database and tables if they do not exist
+    Object.keys(app.services).forEach(path => {
+      const service = app.service(path);
+
+      if(typeof service.init === 'function') {
+        promise = promise.then(() => service.init());
+      }
+    });
+
+    return result;
+  };
 };
