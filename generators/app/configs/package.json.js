0081200161efb4282fc3b9a08e775e4595254658
Christiaan Westerbeek <chris@devotis.nl>
2017-04-19 21:20:13 +0200

    lame commit message as excuse for typo

--- generators/app/configs/package.json.js
+++ generators/app/configs/package.json.js
@@ -18,7 +18,7 @@ module.exports = function(generator) {
     contributors: [],
     bugs: {},
     directories: {
-      lib, testLib: ''
+      lib, libTest: ''
     },
     engines: {
       node: '>= 6.0.0',
