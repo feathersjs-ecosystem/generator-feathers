18cd00d20eb41bd113e31639ce18b63d01116780
Greymouth <thejadenjack@gmail.com>
2017-08-22 12:27:06 -0600

    Added schema template file for generator

---
+++ generators/schema/templates/template.schema.js
@@ -0,0 +1,6 @@
+const Schema = require('feathers-schema').Schema;
+
+// Guide for using schema here: https://www.npmjs.com/package/feathers-schema
+module.exports = new Schema({
+  name: String
+});
