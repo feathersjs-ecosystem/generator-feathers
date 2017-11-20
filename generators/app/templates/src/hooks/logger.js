99f74b6aca6301f4e0896e346d49075dca6c051b
David Luecke <daff@neyeon.com>
2017-11-19 17:41:35 -0800

    Make standard logging hook more compact

--- generators/app/templates/src/hooks/logger.js
+++ generators/app/templates/src/hooks/logger.js
@@ -3,35 +3,15 @@
 // about the logger.
 const logger = require('winston');
 
-// To see more debug messages uncomment the following line
+// To see more detailed messages, uncomment the following line
 // logger.level = 'debug';
-
-// These two methods are recreating a JSON representation of the service call
-// you can customize it to whatever you need
-const json = value => JSON.stringify(value, null, '  ');
-const createMessage = context => {
-  let message = `${context.type} app.service('${context.path}').${context.method}(`;
-
-  if(context.id !== undefined) {
-    message += `${json(context.id)}, `;
-  }
-
-  if(context.data !== undefined) {
-    message += `${json(context.data)}, `;
-  }
-
-  message += `${json(context.params)})`;
-
-  return message;
-};
 
 module.exports = function () {
   return context => {
-    logger.debug(createMessage(context));
-
-    if(context.result !== undefined) {
-      logger.debug(`result: ${json(context.result)}`);
-    }
+    // This debugs the service call and a stringified version of the hook context
+    // You can customize the message (and logger) to your needs
+    logger.debug(`${context.type} app.service('${context.path}').${context.method}()`,
+      JSON.stringify(context));
     
     if (context.error) {
       logger.error(context.error);
