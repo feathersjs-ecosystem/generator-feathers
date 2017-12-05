f43ad81c3950d5e55bf1151fe2b17ade57c811ce
Ray Foss <rayfoss@users.noreply.github.com>
2017-12-05 15:35:50 -0600

    mongodb 3.0.0-rc0 support
    API changes in 3.0 cause this to break. Everything else seems to work flawlessly.

--- generators/connection/templates/mongodb.js
+++ generators/connection/templates/mongodb.js
@@ -2,7 +2,11 @@ const MongoClient = require('mongodb').MongoClient;
 
 module.exports = function (app) {
   const config = app.get('mongodb');
-  const promise = MongoClient.connect(config);
+  const promise = MongoClient.connect(config)
+    .then(client => {
+        if(client.collection) { return client } // mongodb <= 2.2
+        return client.db(client.s.options.dbName)
+      });
 
   app.set('mongoClient', promise);
 };
