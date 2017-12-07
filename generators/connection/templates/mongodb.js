b7e499a27edfd8f65046f056b85ad5be8589261e
David Luecke <daff@neyeon.com>
2017-12-06 18:15:12 -0800

    Fix Linting and use db name parsed from URL

--- generators/connection/templates/mongodb.js
+++ generators/connection/templates/mongodb.js
@@ -1,12 +1,17 @@
+const url = require('url');
 const MongoClient = require('mongodb').MongoClient;
 
 module.exports = function (app) {
   const config = app.get('mongodb');
-  const promise = MongoClient.connect(config)
-    .then(client => {
-        if(client.collection) { return client } // mongodb <= 2.2
-        return client.db(client.s.options.dbName)
-      });
+  const dbName = url.parse(config).path.substring(1);
+  const promise = MongoClient.connect(config).then(client => {
+    // For mongodb <= 2.2
+    if(client.collection) {
+      return client
+    }
+    
+    return client.db(dbName)
+  });
 
   app.set('mongoClient', promise);
 };
