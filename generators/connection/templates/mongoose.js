a87fe1eab18ce2de7022330506e106a82f2ab4f3
Michael Cole <github@michaelcole.com>
2019-06-03 21:33:29 -0700

    If mongoose can't connect, the process is unhealthy and should terminate by default (#420)

--- generators/connection/templates/mongoose.js
+++ generators/connection/templates/mongoose.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const logger = require('./logger');
 
 module.exports = function (app) {
   mongoose.connect(
     app.get('mongodb'),
     { useCreateIndex: true, useNewUrlParser: true }
-  );
+  ).catch(err => {
+    logger.error(err);
+    process.exit(1);
+  });
+  
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
