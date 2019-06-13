3f0c37a803d9cb13782582d2d3239b9f1052c011
Ucok Freddy <ucokfm@gmail.com>
2019-06-13 23:09:24 +0700

    Update index.ts of middleware and services

--- generators/app/templates/src-ts/middleware/index.ts
+++ generators/app/templates/src-ts/middleware/index.ts
@@ -1,6 +1,6 @@
-import { Application } from '@feathersjs/feathers';
+import { Application } from '@feathersjs/express';
 
 export default function (app: Application) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
-};
+}
--- generators/app/templates/src-ts/services/index.ts
+++ generators/app/templates/src-ts/services/index.ts
@@ -1,4 +1,4 @@
 import { Application } from '@feathersjs/feathers';
 
 export default function (app: Application) {
-};
+}
