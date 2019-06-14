83ba45ea5a81017f4e932033bbe109598e9ee4b0
Ucok Freddy <ucokfm@gmail.com>
2019-06-14 23:45:37 +0700

    Create connection/templates-ts

---
+++ generators/connection/templates-ts/mongoose.ts
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const logger = require('./logger');
+
+module.exports = function (app) {
+  mongoose.connect(
+    app.get('mongodb'),
+    { useCreateIndex: true, useNewUrlParser: true }
+  ).catch(err => {
+    logger.error(err);
+    process.exit(1);
+  });
+  
+  mongoose.Promise = global.Promise;
+
+  app.set('mongooseClient', mongoose);
+};
