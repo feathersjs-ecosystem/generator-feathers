9fa030280b771e21013d9aa1f18bb1de9a653c14
Ucok Freddy <ucokfm@gmail.com>
2019-06-16 22:44:00 +0700

    Enable authentication in meta.js

--- meta.js
+++ meta.js
@@ -1,5 +1,6 @@
 module.exports = {
   app: 'Create a new Feathers application in the current folder',
+  authentication: 'Set up authentication for the current application',
   connection: 'Initialize a new database connection',
   hook: 'Create a new hook',
   middleware: 'Create an Express middleware',
