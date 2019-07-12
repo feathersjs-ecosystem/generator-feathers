4b9dc07d85ea01e32c40a6dc4d84cfb15eae6197
David Luecke <daff@neyeon.com>
2019-07-12 15:26:18 -0700

    Unify static folder and remove adapter declaration that have types

--- generators/app/templates/ts/src/declarations.d.ts
+++ generators/app/templates/ts/src/declarations.d.ts
@@ -1,12 +1,5 @@
 // List of packages that don't have typings yet
 declare module 'express-cassandra';
 declare module 'feathers-cassandra';
-declare module 'feathers-knex';
-declare module 'feathers-memory';
-declare module 'feathers-mongodb';
-declare module 'feathers-mongoose';
-declare module 'feathers-nedb';
-declare module 'feathers-objection';
-declare module 'feathers-sequelize';
 declare module 'mongodb-core';
 declare module 'nedb';
