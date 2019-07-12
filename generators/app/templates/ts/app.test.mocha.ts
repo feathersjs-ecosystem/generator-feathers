c7a73fecd3adc0afe0ef536ef4403ed0f139ac08
David Luecke <daff@neyeon.com>
2019-07-12 10:52:13 -0700

    Merge with latest master

--- generators/app/templates/ts/app.test.mocha.ts
+++ generators/app/templates/ts/app.test.mocha.ts
@@ -23,7 +23,7 @@ describe('Feathers application tests', () => {
 
   it('starts and shows the index page', () => {
     return rp(getUrl()).then(body =>
-      assert.ok(body.indexOf('<html>') !== -1)
+      assert.ok(body.indexOf('<html lang="en">') !== -1)
     );
   });
 
