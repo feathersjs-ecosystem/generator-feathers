923e398c0aa30344a72451c8cbdd544e91bb5a9e
David Luecke <daff@neyeon.com>
2019-07-21 10:16:31 -0700

    Add compile step to TS tests (#454)

--- generators/connection/templates/ts/mongodb.ts
+++ generators/connection/templates/ts/mongodb.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { parseConnectionString as parse } from 'mongodb-core';
 import { MongoClient } from 'mongodb';
 import { Application } from './declarations';
@@ -6,11 +7,6 @@ const logger = require('./logger');
 export default function (app: Application) {
   const config = app.get('mongodb');
   const promise = MongoClient.connect(config, { useNewUrlParser: true }).then(client => {
-    // For mongodb <= 2.2
-    if((client as any).collection) {
-      return client;
-    }
-
     const dbName = parse(config, () => {});
     return client.db(dbName);
   }).catch(error => {
