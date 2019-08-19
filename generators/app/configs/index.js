5e522994323915de92dcee947454d8e8ea348c8e
Ucok Freddy <ucokfm@gmail.com>
2019-08-19 12:10:03 +0700

    fix: create test.json (#464)

--- generators/app/configs/index.js
+++ generators/app/configs/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   configDefault: require('./config.default.json.js'),
   configProduction: require('./config.production.json.js'),
+  configTest: require('./config.test.json.js'),
   package: require('./package.json.js'),
   eslintrc: require('./eslintrc.json.js'),
   tsconfig: require('./tsconfig.json.js')
