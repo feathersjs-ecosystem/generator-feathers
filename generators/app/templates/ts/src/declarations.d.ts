7897b576d79d91bc05568c25c0bcfde002cf47c7
David Luecke <daff@neyeon.com>
2019-10-29 08:51:26 -0700

    Move transport-commons and remove unnecessary imports

--- generators/app/templates/ts/src/declarations.d.ts
+++ generators/app/templates/ts/src/declarations.d.ts
@@ -1,6 +1,4 @@
 import { Application as ExpressFeathers } from '@feathersjs/express';
-import { Service } from '@feathersjs/feathers';
-import '@feathersjs/transport-commons';
 
 // A mapping of service names to types. Will be extended in service files.
 export interface ServiceTypes {}
