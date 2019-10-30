153ef821cf1a0109ae277f12578328ad53571ddb
David Luecke <daff@neyeon.com>
2019-10-30 08:32:37 -0700

    Update MongoDB intialization

--- generators/connection/templates/js/mongodb.js
+++ generators/connection/templates/js/mongodb.js
@@ -1,20 +1,8 @@
-const parse = require('mongodb-core').parseConnectionString;
 const MongoClient = require('mongodb').MongoClient;
-const logger = require('./logger');
 
 module.exports = function (app) {
   const config = app.get('mongodb');
-  const promise = MongoClient.connect(config, { useNewUrlParser: true }).then(client => {
-    // For mongodb <= 2.2
-    if(client.collection) {
-      return client;
-    }
+  const mongoClient = MongoClient.connect(config, { useNewUrlParser: true });
 
-    const dbName = parse(config, () => {});
-    return client.db(dbName);
-  }).catch(error => {
-    logger.error(error);
-  });
-
-  app.set('mongoClient', promise);
+  app.set('mongoClient', mongoClient);
 };
--- generators/connection/templates/ts/mongodb.ts
+++ generators/connection/templates/ts/mongodb.ts
@@ -1,17 +1,10 @@
-// @ts-ignore
-import { parseConnectionString as parse } from 'mongodb-core';
 import { MongoClient } from 'mongodb';
 import { Application } from './declarations';
-const logger = require('./logger');
 
 export default function (app: Application) {
   const config = app.get('mongodb');
-  const promise = MongoClient.connect(config, { useNewUrlParser: true }).then(client => {
-    const dbName = parse(config, () => {});
-    return client.db(dbName);
-  }).catch(error => {
-    logger.error(error);
-  });
+  
+  const mongoClient = MongoClient.connect(config, { useNewUrlParser: true });
 
-  app.set('mongoClient', promise);
+  app.set('mongoClient', mongoClient);
 }
