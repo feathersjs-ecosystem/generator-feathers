fcb93876d27a6db157f84905f0ad6ab3c7908df5
David Luecke <daff@neyeon.com>
2019-10-30 18:23:14 -0700

    Update MongoDB intialization (#498)

--- generators/connection/templates/js/mongodb.js
+++ generators/connection/templates/js/mongodb.js
@@ -1,20 +1,10 @@
-const parse = require('mongodb-core').parseConnectionString;
 const MongoClient = require('mongodb').MongoClient;
-const logger = require('./logger');
 
 module.exports = function (app) {
-  const config = app.get('mongodb');
-  const promise = MongoClient.connect(config, { useNewUrlParser: true }).then(client => {
-    // For mongodb <= 2.2
-    if(client.collection) {
-      return client;
-    }
+  const connection = app.get('mongodb');
+  const database = connection.substr(connection.lastIndexOf('/') + 1);
+  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
+    .then(client => client.db(database));
 
-    const dbName = parse(config, () => {});
-    return client.db(dbName);
-  }).catch(error => {
-    logger.error(error);
-  });
-
-  app.set('mongoClient', promise);
+  app.set('mongoClient', mongoClient);
 };
--- generators/connection/templates/ts/mongodb.ts
+++ generators/connection/templates/ts/mongodb.ts
@@ -1,17 +1,11 @@
-// @ts-ignore
-import { parseConnectionString as parse } from 'mongodb-core';
 import { MongoClient } from 'mongodb';
 import { Application } from './declarations';
-const logger = require('./logger');
 
 export default function (app: Application) {
-  const config = app.get('mongodb');
-  const promise = MongoClient.connect(config, { useNewUrlParser: true }).then(client => {
-    const dbName = parse(config, () => {});
-    return client.db(dbName);
-  }).catch(error => {
-    logger.error(error);
-  });
+  const connection = app.get('mongodb');
+  const database = connection.substr(connection.lastIndexOf('/') + 1);
+  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
+    .then(client => client.db(database));
 
-  app.set('mongoClient', promise);
+  app.set('mongoClient', mongoClient);
 }
