ce7b482d344581ec3e5439423eeb065b4f21f065
Darren Davison <darren@davisononline.org>
2019-12-07 00:42:44 +0000

    Update mongoose generators for typescript
    In line with the changes in javascript generators, ensure no compilation
    errors when running tests in watch mode.

--- generators/service/templates/ts/model/mongoose.ts
+++ generators/service/templates/ts/model/mongoose.ts
@@ -5,13 +5,19 @@
 import { Application } from '../declarations';
 
 export default function (app: Application) {
+  const modelName = '<%= camelName %>';
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
-  const <%= camelName %> = new Schema({
+  const schema = new Schema({
     text: { type: String, required: true }
   }, {
     timestamps: true
   });
 
-  return mongooseClient.model('<%= camelName %>', <%= camelName %>);
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    mongooseClient.deleteModel(modelName);
+  }
+  return mongooseClient.model(modelName, schema);
 }
