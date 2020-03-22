e5c2dc0072417fae9e8f44588514ffe9a61cfb51
Fratzinger <22286818+fratzinger@users.noreply.github.com>
2020-03-22 15:09:36 +0100

    fix(ts): add Model to sequelize

--- generators/service/templates/ts/model/sequelize-user.ts
+++ generators/service/templates/ts/model/sequelize-user.ts
@@ -1,6 +1,6 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize, DataTypes, Model } from 'sequelize';
 import { Application } from '../declarations';
 import { HookReturn } from "sequelize/types/lib/hooks";
 
