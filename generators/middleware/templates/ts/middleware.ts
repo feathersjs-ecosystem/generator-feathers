93aa89acbc88272741651f035f7dd4c51720d04e
Fratzinger <22286818+fratzinger@users.noreply.github.com>
2020-03-22 13:49:00 +0100

    Revert "fix(ts): add"
    This reverts commit 2a375763168160a39d892355ee52475f59ba2928.

--- generators/middleware/templates/ts/middleware.ts
+++ generators/middleware/templates/ts/middleware.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return (req: Request, res: Response, next: NextFunction) => {
     next();
   };
