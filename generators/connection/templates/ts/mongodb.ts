867300b44673efbea4ac38d4819334580c1fdb8d
Fratzinger <22286818+fratzinger@users.noreply.github.com>
2020-03-22 13:46:21 +0100

    fix(ts): add return types for functions

--- generators/connection/templates/ts/mongodb.ts
+++ generators/connection/templates/ts/mongodb.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 import { Application } from './declarations';
 
-export default function (app: Application) {
+export default function (app: Application): void {
   const connection = app.get('mongodb');
   const database = connection.substr(connection.lastIndexOf('/') + 1);
   const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
--- generators/connection/templates/ts/objection.ts
+++ generators/connection/templates/ts/objection.ts
@@ -1,7 +1,7 @@
 const { Model } = require('objection');
 import { Application } from './declarations';
 
-export default function (app: Application) {
+export default function (app: Application): void {
   const { client, connection } = app.get('<%= database %>');
   const knex = require('knex')({ client, connection, useNullAsDefault: false });
 
