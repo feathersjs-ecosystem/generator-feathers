39ed357f5d9a8fc8d7b8088d49771cf90d1740f3
AON <aon.108@gmail.com>
2021-04-30 14:13:33 +0100

    fix error on new mongodb connection
    `
    (node:2156) [MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
    `

--- generators/connection/templates/js/mongodb.js
+++ generators/connection/templates/js/mongodb.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = function (app) {
   const connection = app.get('mongodb');
   const database = connection.substr(connection.lastIndexOf('/') + 1);
-  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
+  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(client => client.db(database));
 
   app.set('mongoClient', mongoClient);
