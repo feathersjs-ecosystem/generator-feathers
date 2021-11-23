d59ed0b015dd06c7156ad2b10a0ceb6eb68d6d80
AON <aon.108@gmail.com>
2021-11-23 02:58:04 +0000

    fix error on new mongodb connection (#621)

--- generators/connection/templates/ts/mongodb.ts
+++ generators/connection/templates/ts/mongodb.ts
@@ -4,7 +4,7 @@ import { Application } from './declarations';
 export default function (app: Application): void {
   const connection = app.get('mongodb');
   const database = connection.substr(connection.lastIndexOf('/') + 1);
-  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
+  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(client => client.db(database));
 
   app.set('mongoClient', mongoClient);
